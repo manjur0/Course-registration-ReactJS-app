869979c4063ce37751a109fa103f8b0f2676c930
manjur0 <sfmanjur21@gmail.com>
2023-09-15 22:26:25 +0600

    Course name cart section added

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -3,8 +3,13 @@ import PropTypes from 'prop-types';
 
 const Bookmarks = () => {
     return (
-        <div className='w-1/4'>
-            <h3>this is bookmarks</h3>
+        <div className='w-1/4 bg-slate-50 rounded-md  text-black '>
+            <div className='text-left ml-20'>
+                <h2 className='my-2 text-1xl font-bold text-blue-600'>Credit Hour Remaining 7 hr</h2>
+                <hr />
+                <h2 className='mb-2 text-xl font-bold'>Course Name</h2>
+
+            </div>
         </div>
     );
 };
