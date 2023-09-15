1bca25f9bc83191fbd4bce3b3ed771f6b8feeb8f
manjur0 <sfmanjur21@gmail.com>
2023-09-15 21:57:41 +0600

    Price and credit added

--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -2,18 +2,22 @@
 import PropTypes from 'prop-types';
 
 const Course = ({ course }) => {
-    const { img, title, description, } = course
+    const { img, title, description, price, credit } = course
 
     console.log(course);
     return (
         <div className=''>
-          
-          <div className="card w-80 bg-base-100 shadow-xl">
+
+            <div className="card w-80 bg-base-100 shadow-xl">
                 <figure><img className='w-full' src={img} alt={`image of course ${img}`} /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
                     <p>{description}</p>
-                    <div className="card-actions justify-center">
+                    <div className='flex justify-between flex-grow-0'>
+                        <p className='flex-grow-0'> {price}</p>
+                        <p className='flex-grow-0'>Credit: {credit}hr</p>
+                    </div>
+                    <div className="card-actions justify-center ">
                         <button className="btn btn-primary w-full">Select</button>
                     </div>
                 </div>
@@ -23,6 +27,7 @@ const Course = ({ course }) => {
 };
 
 Course.propTypes = {
+    FiBookOpen: PropTypes.func
 
 };
 
